83f8db73bb879a65a40384a04f967c74e10477f6
venktesh.mane18@gmail.com <venktesh.mane18@gmail.com>
2022-03-05 19:36:21 +0530

    First Commit

--- HashTable.java
+++ HashTable.java
@@ -1,23 +1,34 @@
 package HashTable;
-
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.concurrent.ConcurrentMap;
+import java.util.stream.Collectors;
 
-public class frequencyOfWords {
+import static java.util.Arrays.asList;
+public class UC2_FrequencyOfWordsInALargeParagraphPhrase {
 
 	public static void main(String[] args) {
-		String string ="To be or not to be";
-        char[] array = string.toCharArray();
-        HashMap<Character, Integer> hashMap = new HashMap<>();
+		String string =“Paranoids are not paranoid because they are paranoid but because they keep putting themselves
+		        deliberately into paranoid avoidable situations”;
 
-        for(int i = 0; i < array.length; i++) {
-            if(hashMap.containsKey(array[i])) {
-                int count = hashMap.get(array[i]);
-                hashMap.put(array[i], count+1);
-            }
-            else {
-                hashMap.put(array[i], 1);
-            }
-        }
-        System.out.println(hashMap);
-    }
-}
+		        ConcurrentMap<String, Integer> freqMap =
+		                asList(string.split("[\\s.]"))
+		                        .parallelStream()
+		                        .filter(s -> !s.isEmpty())
+		                        .collect(Collectors.toConcurrentMap(w -> w.toLowerCase(), w -> 1, Integer::sum));
+		        System.out.println(freqMap.toString());
+
+		        //Priority queue that uses frequency as the comparator and size as 3
+		        PriorityQueue<String> pq = new PriorityQueue<>(Comparator.comparingInt(freqMap::get));
+		        for (String key : freqMap.keySet()) {
+		            pq.add(key);
+		            if (pq.size() > 3) {
+		                pq.poll();
+		            }
+		        }
+		        String hashCode;
+		        System.out.println(hashCode);
+		    }
+		}
