88c5e45dd0885a65be6119730a8a722efa969724
venktesh.mane18@gmail.com <venktesh.mane18@gmail.com>
2022-03-05 19:49:09 +0530

    First Commit

--- HashTable.java
+++ HashTable.java
@@ -1,34 +1,25 @@
 package HashTable;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.concurrent.ConcurrentMap;
-import java.util.stream.Collectors;
 
-import static java.util.Arrays.asList;
-public class UC2_FrequencyOfWordsInALargeParagraphPhrase {
+public class UC3_RemoveAvoidableWordsFromPhrase {
 
-	public static void main(String[] args) {
-		String string =“Paranoids are not paranoid because they are paranoid but because they keep putting themselves
-		        deliberately into paranoid avoidable situations”;
+	static void remove(String str, String word) {
+        String msg[] = str.split(" ");
+        String new_str = "";
+        for (String words : msg) {
+            if (!words.equals(word)) {
+                new_str += words + " ";
+            }
+        }
+        System.out.print(new_str);
+    }
+    public static void main(String[] args) {
+        String str = "Paranoids are not paranoid because "
+                + "they are paranoid but because they keep "
+                + "putting themselves deliberately into paranoid "
+                + "avoidable situations";
 
-		        ConcurrentMap<String, Integer> freqMap =
-		                asList(string.split("[\\s.]"))
-		                        .parallelStream()
-		                        .filter(s -> !s.isEmpty())
-		                        .collect(Collectors.toConcurrentMap(w -> w.toLowerCase(), w -> 1, Integer::sum));
-		        System.out.println(freqMap.toString());
+        String word = "avoidable";
 
-		        //Priority queue that uses frequency as the comparator and size as 3
-		        PriorityQueue<String> pq = new PriorityQueue<>(Comparator.comparingInt(freqMap::get));
-		        for (String key : freqMap.keySet()) {
-		            pq.add(key);
-		            if (pq.size() > 3) {
-		                pq.poll();
-		            }
-		        }
-		        String hashCode;
-		        System.out.println(hashCode);
-		    }
-		}
+        remove(str, word);
+    }
+}
